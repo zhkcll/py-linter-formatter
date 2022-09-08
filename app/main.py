7d64892cf1e32c602f73bc57979ddd8577bca45d
Nataliia Zakharchuk <62181026+Nattalli@users.noreply.github.com>
2022-09-08 15:49:27 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 def format_linter_error(error: dict) -> dict:
-    # white your code here
+    # write your code here
     pass
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # white your code here
+    # write your code here
     pass
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # white your code here
+    # write your code here
     pass
